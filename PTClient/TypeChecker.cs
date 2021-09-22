4bed35f3dc61fad6766744fdabfa34a957808568
unknown <poudynpro@gmail.com>
2021-09-22 22:23:14 +0330

    adding type checker

---
+++ PTClient/TypeChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTClient
+{
+    internal static class TypeChecker
+    {
+        internal static bool IsUpdate(this Type updateType)
+        {
+            return updateType == typeof(Telegram.Td.Api.Update);
+        }
+        internal static bool IsBaseObject(this Type updateType)
+        {
+            return updateType == typeof(Telegram.Td.Api.BaseObject);
+        }
+    }
+}
